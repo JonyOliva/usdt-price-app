0ec884a1557a107cf97b30a2f4d802311693a4ed
JOliva <joliva@dikter.com.ar>
2022-11-27 16:54:18 -0300

    quito console log

--- web-app/src/dao/database.js
+++ web-app/src/dao/database.js
@@ -1,21 +1,13 @@
 import mariadb from 'mariadb';
 
 const pool = mariadb.createPool({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    port: process.env.DB_PORT,
-    password: process.env.DB_PWD,
-    database: process.env.DB_NAME
-  });
-
-console.log({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   port: process.env.DB_PORT,
   password: process.env.DB_PWD,
   database: process.env.DB_NAME
-})
+});
 
 export const getConn = async () => {
-    return pool.getConnection();
+  return pool.getConnection();
 }
